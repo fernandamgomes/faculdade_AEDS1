d65132b9eb25bc367f1e8e2eca5a4aa06eaffe29
Fernanda <89420917+fernandamgomes@users.noreply.github.com>
2021-09-20 09:56:08 -0300

    Add files via upload

---
+++ lista03/ex05.c
@@ -0,0 +1,74 @@
+/* 
+Fernanda Mendes Gomes (17/09/2021) // (20/09/2021)
+Faça  um  procedimento  que  recebe  a  média  final  de  um  aluno,  identifica  e  exibe  o  seu 
+conceito,  conforme  a  tabela  abaixo.  
+Faça  um  programa  que  leia  a  média  de  N  alunos, 
+acionando o procedimento para cada um deles. 
+N deve ser lido do teclado
+
+Nota Conceito 
+    Até 39 - F 
+    40 a 59 -  E 
+    60 a 69 -  D 
+    70 a 79 - C 
+    80 a 89 - B 
+    A partir de 90 - A
+*/
+
+#include <stdio.h>
+void conceito (float media); 
+int main(void)
+{
+    // VARIAVEIS
+    int N = 0; 
+    float media = 0.0; 
+
+    // input do usuario: numero de alunos (quantas vezes o programa vai analisar as notas)
+    printf("Qual o numero de alunos: ");
+    scanf("%d", &N);
+    printf("\n"); 
+
+    // loop que roda N vezes (numero de alunos)
+    for (int i = 0; i < N; i = i + 1)
+    {
+        // input do usuario: receber valor da media do aluno 
+        printf("Qual a media do aluno %d : ", (i+1));
+        scanf("%f", &media); 
+        // chamar procedimento 
+        conceito(media); 
+        // proximo aluno
+    }
+    return 0; 
+}
+
+// procedimento que recebe a media, identifrica e exibe o conceito
+void conceito(float media)
+{
+    // analisar e imprimir conceito baseado no valor de media recebido pelo procedimento
+    printf("Conceito: "); 
+    if (media <= 39) 
+    {
+        printf("F\n"); 
+    }
+    else if ((media >= 40) && (media <= 59))
+    {
+        printf("E\n"); 
+    }
+    else if ((media >= 60) && (media <= 69))
+    {
+        printf("D\n"); 
+    }
+    else if ((media >= 70) && (media <= 79))
+    {
+        printf("C\n"); 
+    }
+    else if ((media >= 80) && (media <= 89))
+    {
+        printf("B\n"); 
+    }
+    else if (media >= 90)
+    {
+        printf("A\n");
+    }
+    printf("-------------------------------\n");
+}
---
+++ lista03/ex06.c
@@ -0,0 +1,57 @@
+/* 
+Fernanda Mendes Gomes (17/09/2021) // (17/09/2021)
+Escreva uma função que recebe por parâmetro um valor inteiro e positivo N e retorna o 
+valor de S, calculado segundo a fórmula abaixo.  
+    S = 1 + 1/1! + 1/2!  + 1/3! + ...+1 /N! 
+Faça um programa que leia N e imprima o valor retornado pela função.
+*/
+
+#include <stdio.h>
+float equacao (int num_positivo); 
+float fatorial (int num); 
+int main(void)
+{
+    // VARIAVEIS
+    int N = 0; 
+    // forcar o usuario a fornecer um numero inteiro positivo
+    while (N<=0)
+    {
+        // input do usuario: numero inteiro positivo
+        printf("Digite um valor inteiro positivo "); 
+        scanf("%d", &N); 
+        // chamar funcao que calcula S
+        printf("%.5f", equacao(N)); 
+    }
+    return 0; 
+}
+
+// funcao que calcula o fatorial
+float fatorial (int num)
+{
+    // base case
+    if (num==1)
+    {
+        return 1.0; 
+    }
+    // esgotar funcao 
+    else
+    {
+        return num * fatorial(num - 1.0); 
+    }
+
+}
+// funcao que calcula S 
+float equacao (int num_positivo)
+{
+    // base case
+    if (num_positivo == 1)
+    {
+        return 2.0; 
+    }
+    // esgotar funcao 
+    else
+    {
+        return 1.0/fatorial(num_positivo) + equacao (num_positivo - 1); 
+    }
+}
+
